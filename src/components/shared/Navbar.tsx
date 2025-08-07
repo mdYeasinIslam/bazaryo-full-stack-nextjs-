643bcdb1bc35fce4f198b3444e104d8e0c06b1b5
mdYeasinIslam <hyeasinislam@gamil.com>
2025-08-07 12:09:27 +0600

    navbar complete

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -27,11 +27,11 @@ const Navbar = () => {
 
     return (
       <nav className="bg-white shadow-md fixed w-full z-50">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="max-w-7xl mx-auto px-4 sm:px-2 lg:px-8">
           <div className="flex items-center justify-between h-16">
             {/* Left: Logo & Mobile Menu */}
-            <div className="flex items-center">
-              <Link href="/home" className="flex items-center">
+            <div className="flex flex-row-reverse items-center gap-2">
+              <Link href="/" className="flex items-center">
                 <Image
                   src={logo}
                   alt="shop logo"
@@ -48,7 +48,7 @@ const Navbar = () => {
               {/* Mobile menu button */}
               <button
                 onClick={() => setShow(!show)}
-                className="ml-4 lg:hidden text-2xl text-gray-700 focus:outline-none"
+                className=" lg:hidden text-2xl text-gray-700 focus:outline-none"
                 aria-label="Toggle Menu"
               >
                 {show ? <RxCross2 /> : <AiOutlineMenuUnfold />}
@@ -56,12 +56,12 @@ const Navbar = () => {
             </div>
 
             {/* Center: Menu (Desktop) */}
-            <div className="hidden lg:flex lg:gap-5">
+            <div className="hidden lg:flex items-center md:gap-4 lg:gap-5 xl:gap-5">
               {menuItems.map((item) => (
                 <Link
                   key={item.href}
                   href={item.href}
-                  className="text-gray-700 hover:text-[var(--primary-color)] transition font-semibold px-2 py-1 rounded-md"
+                  className="text-gray-700 hover:text-[var(--primary-color)] transition font-medium  py-1 rounded-md"
                 >
                   {item.label}
                 </Link>
@@ -69,9 +69,9 @@ const Navbar = () => {
             </div>
 
             {/* Right: Search & Auth */}
-            <div className="flex items-center space-x-4">
+            <div className="flex items-center gap-1 md:gap-2">
               <BiSearchAlt2 className="w-6 h-6 text-slate-800 hover:text-[var(--primary-color)] cursor-pointer" />
-              {user?.email ? (
+              {!user?.email ? (
                 <Link
                   href="/logIn"
                   onClick={signOut}
@@ -83,13 +83,13 @@ const Navbar = () => {
                 <>
                   <Link
                     href="/signUp"
-                    className="text-green-800 font-semibold px-3 py-2 rounded-md hover:bg-blue-50 transition"
+                    className="text-green-800 bg-green-100 font-semibold px-2 md:px-3 py-2 rounded-md hover:bg-green-200 transition"
                   >
                     Sign Up
                   </Link>
                   <Link
                     href="/logIn"
-                    className="bg-green-800 text-white px-4 py-2 rounded-md font-semibold hover:bg-blue-700 transition"
+                    className="bg-green-800 text-white px-4 py-2 rounded-md font-semibold hover:bg-[var(--hover-color)] transition"
                   >
                     Log In
                   </Link>
@@ -114,15 +114,15 @@ const Navbar = () => {
                   </Link>
                 </li>
               ))}
-              <li className="mt-2">
-                {user?.email ? (
+              {/* <li className="mt-2">
+                {!user?.email ? (
                   <Link
                     href="/logIn"
                     onClick={() => {
                       signOut();
                       setShow(false);
                     }}
-                    className="block  text-white px-4 py-2 rounded-md font-semibold  transition"
+                    className="block  text-black px-4 py-2 rounded-md font-semibold  transition"
                   >
                     Log Out
                   </Link>
@@ -130,21 +130,21 @@ const Navbar = () => {
                   <div className="flex flex-col space-y-2">
                     <Link
                       href="/signUp"
-                      className="block text-blue-600 font-semibold px-3 py-2 rounded-md hover:bg-blue-50 transition"
+                      className="block bg-green-100 text-[var(--primary-color)] font-semibold px-3 py-2 rounded-md hover:bg-green-200 transition"
                       onClick={() => setShow(false)}
                     >
                       Sign Up
                     </Link>
                     <Link
                       href="/logIn"
-                      className="block bg-blue-600 text-white px-4 py-2 rounded-md font-semibold hover:bg-blue-700 transition"
+                      className="block bg-[var(--primary-color)] text-white px-4 py-2 rounded-md font-semibold hover:bg-[var(--hover-color)] transition"
                       onClick={() => setShow(false)}
                     >
                       Log In
                     </Link>
                   </div>
                 )}
-              </li>
+              </li> */}
             </ul>
           </div>
         )}
